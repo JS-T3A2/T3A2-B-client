3cd61952cb67501e85e82b3e6ef3f94868cb0dea
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2021-07-10 22:10:25 +1000

    Will be adding routes.

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,32 @@
 import { useQuery } from '@apollo/client';
+import jwt_decode from 'jwt-decode';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import { IS_AUTH } from '@utils';
+import { useActions } from '@hooks';
+import { getStoredAuthToken, IS_AUTH } from '@utils';
 
 const App = () => {
+  const { setAdminData } = useActions();
   const { data, error, loading } = useQuery(IS_AUTH);
   const { adminData } = useSelector(({ app }) => app);
 
   useEffect(() => {
     if (data && !error && !loading) {
-      console.log(data);
+      if (data.isAuth) {
+        console.log(data);
+        const tokenPayload = jwt_decode(getStoredAuthToken());
+        setAdminData(tokenPayload);
+      } else {
+        console.log('GO to atuyh');
+      }
     }
   }, [data, error, loading]);
 
   return (
     <div>
       <h1>Learn React</h1>
-      {loading ? 'Loading...' : <h2>{adminData}</h2>}
+      {loading ? 'Loading...' : <h2>{adminData?.username}</h2>}
     </div>
   );
 };
